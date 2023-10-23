2a2b73758eb95780048f200b14514a05a889a09b
112ab0058 <146065141+112ab0058@users.noreply.github.com>
2023-10-24 02:36:28 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -5,11 +5,18 @@ int main()
     int s1,d1,s2,d2,s3,d3;
     scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
     int num=1;
-    if(d1<=s1){
+    if((d1<=s2)||(d1<=s3)){
+        num+=0;
+    }else{
         num+=1;
-    }if(d2<=s3){
+    }if((d2<=s3)||(d2<=s1)){
+        num+=0;
+    }else{
         num+=1;
-    }if(d1<=s3){
+    }
+    if((d1<=s3)||(d1<=s2)){
+        num+=0;
+    }else{
         num+=1;
     }
     printf("%d",num);

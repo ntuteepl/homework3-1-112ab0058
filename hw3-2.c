cb0d8d08fbd8df7ea27d5a8dd5cf6bb0c5831715
112ab0058 <146065141+112ab0058@users.noreply.github.com>
2023-10-24 02:45:32 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    int s1, d1, s2, d2, s3, d3;
-    scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
-
-    int num = 1; 
-    if (d1 < s2) {
-        num += 0; 
-    } else if (d2 < s3) {
-        num += 0; 
-    } else {
-        num += 1; 
+int main()
+{
+    int s1,d1,s2,d2,s3,d3;
+    scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
+    int num=1;
+    if(d1<=s2){
+        num+=0;
+    }else if(d2<=s3){
+        num+=0;
+    }else{
+        num+=1;
     }
-
-    printf("%d", num);
-    return 0;
+    printf("%d",num);
 }

dad3e5228f8a904561986105df891eaaa515532e
112ab0058 <146065141+112ab0058@users.noreply.github.com>
2023-10-24 02:39:42 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
-    int s1,d1,s2,d2,s3,d3;
-    scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
-    int num=1;
-    if((d1<=s2)||(d1<=s3)){
-        num+=0;
-    }else{
-        num+=1;
-    }if((d2<=s3)||(d2<=s1)){
-        num+=0;
-    }else{
-        num+=1;
+int main() {
+    int s1, d1, s2, d2, s3, d3;
+    scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
+
+    int num = 1; // 初始至少需要一辆车
+
+    if (d1 < s2) {
+        num += 0; // 第二个任务可以在第一个任务结束后开始
+    } else {
+        num += 1; // 需要多一辆车
     }
-    if((d1<=s3)||(d1<=s2)){
-        num+=0;
-    }else{
-        num+=1;
+    if (d2 < s3) {
+        num += 0; // 第三个任务可以在第二个任务结束后开始
+    } else {
+        num += 1; // 需要多一辆车
     }
-    printf("%d",num);
+
+    printf("%d\n", num);
+    return 0;
 }
